788b51e66beaeb66f7e736c025fe197a60228efe
m1stborn <r10922178@ntu.edu.tw>
2022-03-19 16:54:24 +0800

    Feat: add slot tagging checkpoint and script

---
+++ HW1/slot_tag.sh
@@ -0,0 +1,5 @@
+python3.8 test_slot.py --test_file "${1}" --ckpt_path ckpt/slot/Slot-e472c610.pt --pred_file "${2}"
+
+# Local
+# bash slot_tag.sh .\data\slot\test.json slot.resutl.csv
+# python test_slot.py --test_file "${1}" --ckpt_path ckpt/slot/Slot-e472c610.pt --pred_file "${2}"
