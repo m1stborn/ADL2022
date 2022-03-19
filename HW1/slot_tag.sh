b63f261a29f2f051f3d5703398d43f78cb8b16c5
m1stborn <r10922178@ntu.edu.tw>
2022-03-19 17:13:52 +0800

    Feat: add embedding.pt and vocab.pkl

--- HW1/slot_tag.sh
+++ HW1/slot_tag.sh
@@ -1,5 +1,3 @@
-wget https://www.dropbox.com/s/qko2m2ohqr7rtw2/Slot-e472c610.pt?dl=1 -O Slot-e472c610.pt
-
 python3.8 test_slot.py --test_file "${1}" --ckpt_path Slot-e472c610.pt --pred_file "${2}"
 
 # Local
