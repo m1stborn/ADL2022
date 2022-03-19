6f9d50f7917522eb3233901fe2962fac32b0e4e3
m1stborn <r10922178@ntu.edu.tw>
2022-03-19 16:13:37 +0800

    Feat: add intent classifier checkpoint and script

--- HW1/intent_cls.sh
+++ HW1/intent_cls.sh
@@ -1,1 +1,5 @@
-python3.8 test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/best.pt --pred_file "${2}"
+#python3.8 test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/Intent-9927f1a9.pt --pred_file "${2}"
+
+# Local
+bash intent_cls.sh .\data\intent\test.json intent.resutl.csv
+python test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/Intent-9927f1a9.pt --pred_file "${2}"
